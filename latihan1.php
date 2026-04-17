f5390ff6f5b2470abfd3a6ac9d5e2f6589bb9e68
mrieaziz <mrieaziz@gmail.com>
2026-04-17 15:04:18 +0700

    latihan1.phpt

--- latihan1.php
+++ latihan1.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Latihan</title>
 </head>
 <body>
-    
+    <h1>Latihan1</h1>
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>Usia</th>
+            <th>no hp</th>
+        </tr>
+        
+        <tr>
+            <td>Nabil</td>
+            <td>30</td>
+            <td>08123456789</td>
+        </tr>
+        </tr>
+        <tr>
+            <td>Nabi</td>
+            <td>19</td>
+            <td>08123456789</td>
+        </tr>
+    </table>
 </body>
 </html>
